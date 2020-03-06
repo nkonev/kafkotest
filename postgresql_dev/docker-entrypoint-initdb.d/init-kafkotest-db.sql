8cb0b72ee8ea99a7288be3211826f7c24e4d5764
Nikita Konev <nikit.cpp@yandex.ru>
2020-03-06 11:38:10 +0300

    rm postgres tuning

--- postgresql_dev/docker-entrypoint-initdb.d/init-kafkotest-db.sql
+++ postgresql_dev/docker-entrypoint-initdb.d/init-kafkotest-db.sql
@@ -4,17 +4,17 @@
 -- ALTER SYSTEM SET synchronous_commit = 'off'; -- https://postgrespro.ru/docs/postgrespro/9.5/runtime-config-wal.html#GUC-SYNCHRONOUS-COMMIT
 -- ALTER SYSTEM SET shared_buffers='512MB';
 -- ALTER SYSTEM SET fsync=FALSE;
-ALTER SYSTEM SET full_page_writes=FALSE;
-ALTER SYSTEM SET commit_delay=100000;
-ALTER SYSTEM SET commit_siblings=10;
-ALTER SYSTEM SET work_mem='256MB';
+-- ALTER SYSTEM SET full_page_writes=FALSE;
+-- ALTER SYSTEM SET commit_delay=100000;
+-- ALTER SYSTEM SET commit_siblings=10;
+-- ALTER SYSTEM SET work_mem='256MB';
 
 create user kafkotest with password 'kafkotestPazZw0rd';
 create database kafkotest with owner kafkotest;
 
 \connect kafkotest;
 
-create extension if not exists "uuid-ossp" schema pg_catalog;
+-- create extension if not exists "uuid-ossp" schema pg_catalog;
 -- create extension if not exists "hstore" schema pg_catalog;
 
 -- https://www.endpoint.com/blog/2012/10/30/postgresql-autoexplain-module
